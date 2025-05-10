e0a5ebcd4e62f1ce3f65f73f8a56b12f0b90b9b4
HenkJP <henkjanp.dev@gmail.com>
2025-05-10 16:12:00 +0200

    start

--- index.php
+++ index.php
@@ -8,9 +8,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href="css/main.css">
 </head>
 
 <body>
+    <header>
+        <div class="header">
+            <img src="images/logo.png" alt="Logo of Sap Coaching">
+            <h1>Sap Coaching</h1>
+        </div>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
 
 </body>
 
