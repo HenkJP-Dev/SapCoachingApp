a047d5773656ecd00148c62b01c54c3312e2ade7
HenkJP <henkjanp.dev@gmail.com>
2025-05-26 23:06:54 +0200

    Popup prototype added

--- js/home.js
+++ js/home.js
@@ -1,11 +1,31 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', init);
 
+function init() {
     const hamburgerMenu = document.querySelector('.hamburger-menu');
 
     const sideNav = document.querySelector('.sideNav');
+
+    const openOffer = document.querySelector('.openOffer');
+    const closeOffer = document.querySelector('.closeOffer')
+    const offerPopup = document.querySelector('.offerPopup')
 
     hamburgerMenu.addEventListener('click', () => {
         hamburgerMenu.classList.toggle('active');
         sideNav.classList.toggle('active');
     })
-})
+
+    openOffer.addEventListener('click', () => {
+        offerPopup.classList.toggle('open');
+    })
+
+    closeOffer.addEventListener('click', () => {
+        offerPopup.classList.toggle('open');
+    })
+
+    offerPopup.addEventListener('click', (e) => {
+        if (e.target === offerPopup) {
+            offerPopup.classList.remove('open');
+        }
+    });
+}
+
